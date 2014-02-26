0739376bdf522a224ffe18f2b3eeb52627bc3da7
Rory Duncan <robust.rory@gmail.com>
2014-02-26 13:08:06 -0800

    Increase accuracy of event calls due to delta timing.

--- example/ClockEventAggreggator.js
+++ example/ClockEventAggreggator.js
@@ -3,7 +3,6 @@
 (function(window, undefined) {
 
     /*  Request Animation polyfill  */
-
     //  http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
 
 
@@ -31,12 +30,11 @@
         };
 
 
-
   var ClockEventAggreggator = function(args) {
 
+    /* Helpers */
 
-    /* Helper for merging objects */
-
+    // Helper for merging objects
     var extend = function(){
       if (arguments.length < 2) return;
       var extended = arguments[0];
@@ -49,30 +47,56 @@
       return extended;
     };
 
+    var isArray = function(thing){
+      // assume 'thing' is not undefined or null as it will throw a TypeError.
+      if (typeof thing === "string") return false;
+      if ((thing).length === undefined) return false;
+      else return true; 
+    }
+
 
     /*  Timeline related variables  */
 
     var defaults = {useRAF: true, tickInterval: 16, autostart: false},
         options = extend(defaults, args),
         ticks = 0,
-        lastTick = 0,       // used to calculate the delta
+        lastTick = 0,
         startTime = 0,
-        elapsedTime = 0,
-        elapsedSeconds = 0, // to be refactored
-        rAFID = null,
-        tick2N = false,
 
-        delta = 0,          // the time difference between ticks, useful for adjustments 
+        // the 'clock' time, calculated with delta and clockSpeed variables.
+        elapsedTime = 0,
+
+        // id for requestAnimationFrame
+        rAFID = null,
+
+        // whether this is every other tick
+        tick_n = false,
+
+        // the amount of real-time that has passed between ticks
+        delta = 0,
         actualElapsedTime = 0;
+
+        // fallback time for setTimeout
         this.tickInterval = options.tickInterval,
 
         running = false,
 
+        // speed at which the clock runs
+        clockspeed = 1,
+
+        // reference
+        self = this,
+
         // wether or not to use request Animation Frame
         useRAF = options.useRAF,
-        tickCallee = null,
 
-        clockspeed = 0.1;
+        // dynamic function for rAF or setTimeout functionality,
+        tickCallee = function() {
+          // set to start() initially in case called before start somehow.
+          this.start();
+        };
+
+        
         
 
 
@@ -80,7 +104,7 @@
 
     var events = {};
         events.nominal = {};
-        events.ordinal = [];
+        events.ordinal = {};
         events.loops = {};
 
     
@@ -93,15 +117,15 @@
       if (!running) return;
       
       ticks += 1 * clockspeed;
-      tick2N = !tick2N;
-      if (this.debug && tick2N) this.log();
+      tick_n = !tick_n;
+      if (this.debug && tick_n) this.log();
 
       
-      this.delta = delta = now - lastTick;
+      this.delta = delta = (now - lastTick) * clockspeed;
 
       lastTick = now;
 
-      elapsedTime += (delta * clockspeed);
+      elapsedTime += delta;
 
       actualElapsedTime = ( now - startTime );
 
@@ -121,25 +145,28 @@
     };
     
     var buildDefaultEvents = function() {
-      // You would think that a programmer would iterate an array to make these.
-      events.nominal["tick"] = [];
 
-      events.nominal["start"] = [];
-      events.nominal["after:start"] = [];
+      var defaultEvents = ["tick", "start", "after:start", "pause", "resume"],
+          addEvent = function(name){
 
-      events.nominal["pause"] = [];
+            events.ordinal[name] = [];
+          };
 
-      events.nominal["resume"] = [];
+      defaultEvents.forEach(addEvent);
+
     };
-    // trigger's any .on() or .at() events
+    buildDefaultEvents();
+
     var triggerOrdinalEvents = function() {
 
-      var now = ~~(elapsedTime / 1000),
-          eot = events.ordinal[ now ]; //shorthand
+      var now = ~~(elapsedTime / 100) * 100;
 
-      if ( events.ordinal[ now ] === undefined ) return false;
+      if ( events.ordinal[ now ] === undefined ) {return false;}  // return false in case of checks
+
       
-      if (this.debug) console.log("Triggering all events at", now, "seconds.");
+      var eot = events.ordinal[ now ];
+
+      // events.ordinal will be an array, so loop over each function in it.
       for (var _x = 0, _xx = eot.length; _x < _xx; _x++) {
 
         var fn = eot[_x].fn,
@@ -147,35 +174,37 @@
             args = eot[_x].args;
 
         fn.apply(ctx, args);
-
       }
+
       // delete it, so that it won't trigger the next ticks that round down to this time.
-      delete events.ordinal[now];
-      
-      return true;
+      delete events.ordinal[ now ];
+
+      return true; // return true in case of checks
     };
 
     var triggerLoopEvents = function() {
 
-      var now = ~~(elapsedTime / 1000);
+      var now = ~~elapsedTime;
+
       var expired = [];
 
       for (var key in events.loops) {
 
         var loop = events.loops[ key ]
 
-        if (loop.check(now) &&          // Check whether the loop should fire at the current time
-            loop.lastCalled !== now &&  // Check if it has already fired during this second
-            loop.start <= now &&        // Check if it is 'allowed' to start.
-            loop.delete === false) {    // Check if it is to be deleted
+        if (loop.start <= now &&          // Check if it is 'allowed' to start.
+            loop.checkInterval( now ) &&  // Check whether the loop should fire at the current time
+            loop.delete === false ) {      // Check if it is to be deleted
             
             // there is a reference to _this_ inside of loop,
             // so sending the loop itself as context is enough.
-            loop.calledAt.push(now);
+            loop.calledAt.push( now );
             loop.now = now;
-            loop.fn.apply(loop, loop.args);
-            loop.lastCalled = now;
-            loop.calls++; 
+            
+            loop.fn.apply( loop, loop.args );
+            
+            loop.lastCall = now;
+            loop.calls++;
         }
         // check whether it has expired
         if (loop.stop === now || loop.calls === loop.maxIntervals || loop.delete === true) {
@@ -209,31 +238,37 @@
 
     this.start = function() {
 
-      var tickLoop;
-      //dynamically constructed function to remove an if statement inside of the tick function.
+      if (running === true) { return this; } // simple tests to prevent multiple starts
+
+      // tickLoop is a dynamically constructed function to remove an 'if' statement inside of tick() .
+      var tickLoop;  
+
       if (useRAF === true) {
         tickLoop = new Function(" var id = window.requestAnimationFrame( this.tick ); return id;");
       }
       else {
         tickLoop = new Function(" var id = window.setTimeout(this.tick, this.tickInterval);return id;" );
       }
-
+      // bind into this context
       tickCallee = tickLoop.bind(this);
 
       this.trigger("before:start");
-      var now = new Date().getTime();
 
+      var now = new Date().getTime();
       this.startTime = startTime = now;
       running = true;
       lastTick = now;
       this.tick();
 
+      this.enablePauseOnBlur();
       this.trigger("start");
+
       return this;
-    };
+    }.bind(this);
 
-    this.reset = function(){
+    this.stop = function(){
 
+      running = false;
       //reset event lists
       events.nominal = {};
       events.ordinal = [];
@@ -243,26 +278,33 @@
 
       //reset counters
       ticks = 0;
+      elapsedTime = 0;
       this.startTime = startTime = 0;
 
-      //boot up
+      return this;
+    }.bind(this);
+
+    this.reset = function(){
+
+      this.reset();
       this.start();
       return this;
-    };
+    }.bind(this);
 
     this.pause = function() {
       running = false;
       this.trigger("pause");
       return this;
-    };
+    }.bind(this);
 
     this.resume = function() {
       if (running === true) return this;
       running = true;
+      lastTick = new Date().getTime();
       this.tick();
       this.trigger("resume");
       return this;
-    };
+    }.bind(this);
 
     this.clockScalar = function(multiplier) {
       // a relative way to affect the clock
@@ -274,13 +316,30 @@
       clockspeed = value || clockspeed;
     };
 
-    this.ticks = function(){return ticks;};
+    this.enablePauseOnBlur = function() {
+      // called on start
+      // requestAnimationFrame natively pauses onblur, but, this clock doesn't
+      // so I've programmed the functionality into it. 
 
-    this.seconds = function(){return elapsedTime / 10000 ;};
+      window.onblur = function(){self.pause();}
+      window.onfocus = function(){self.resume();};
+    };
 
+    this.disablePauseOnBlur = function(){
+      window.onblur = function(){return null};
+      window.onfocus = function(){return null};
+    };
     
-    
+    this.ticks = function(){ return ticks; };
+
+    this.milliseconds = function(){ return elapsedTime;};
+
+    this.seconds = function(){ return elapsedTime / 1000 ; };
+
+    this.minutes = function(){ return (this.seconds() / 60); };
+
     this.debug = options.debug || false;
+
     // if debug is true, log is automatically called each tick
     this.log = function() {
 
@@ -291,7 +350,7 @@
       console.log("delta:", delta);
       console.log("lastTick(should change)", ~~lastTick)
       console.log("elapsed time:", elapsedTime / 1000);
-      console.log("elapsed seconds:", ~~(elapsedTime / 10000));
+      console.log("elapsed seconds:", ~~(elapsedTime / 1000));
       console.log("actual elapsed time:", actualElapsedTime / 1000);
       console.warn("%cdifference (in seconds): " + (this.errorMargin / 1000), "color: #a00" );
       console.log("Ignore differences if you have called pause at any time.");
@@ -304,7 +363,6 @@
 
     /*  Event-related methods  */
     
-
     this.trigger = function(eventName) {
 
       if (events.nominal[eventName] === undefined) return this;
@@ -324,7 +382,7 @@
       return this;
     };
 
-    this.on = function(eventName, fn /* [, args, context ] */) {
+    this.on = function( eventName, fn /* [, args, context ] */ ) {
 
       var context = arguments[3] || this, args = arguments[2] || [];
 
@@ -341,47 +399,106 @@
       return this;
     };
 
-    this.at = function( elapsedSeconds, fn /* [, args, context ] */) {
-
-      if (elapsedSeconds === undefined) {throw new Error("at() method requires a clock-time passed in as first parameter."); return;}
+    this.at = function( elapsedMS, fn /* [, args, context ] */ ) {
+      if (elapsedMS === undefined) {throw new Error("at() method requires a clock-time passed in as first parameter."); return;}
       if (fn === undefined) {throw new Error("at() method requires a function passed in as second parameter."); return;}
+
       var context = arguments[3] || null, args = arguments[2] || this;
 
-      events.ordinal[elapsedSeconds] = events.ordinal[elapsedSeconds] || [];
-      events.ordinal[elapsedSeconds].push({
-        "fn": fn,
-        "context": context,
-        "args": args
-      });
+      var addTime = function( time ) {
 
-      return this;
+        var t = time.toString();
+        events.ordinal[ time ] = events.ordinal[ time ] || [];
+        events.ordinal[ time ].push({
+          "fn": fn,
+          "context": context,
+          "args": args
+        });
+
+      };
+
+      if ( !isArray(elapsedMS) ) {
+        addTime( elapsedMS );
+        
+        return this;
+
+      }
+
+        elapsedMS.forEach( addTime );
+
+
+        return this;
     };
 
-    this.loop = function(uniqueName, fn, args, scope ) {
+    this.after = function( milliseconds, fn /* [, args, context ] */ ) {
+      var args = arguments[2], context = arguments[3];
+      this.at( elapsedTime + milliseconds, fn, args, context );
+    };
+
+    this.loop = function( uniqueName, fn, args, scope ) {
+
+      /*
+          this.loop() returns a new loop object, which to alter timing-related options
+          you call it's .for() method. 
+          
+          EX:
+          var x = this.loop(*args).for(*args);
+      */
 
       if (!uniqueName) return this;
 
       var self = this,
-          defaults = { "start": ~~elapsedSeconds, "stop": Infinity, "interval": 9999, "maxIntervals": Infinity, "duration": 0, "calledAt": [] },
-          required = { "name":uniqueName, "fn": fn, "calls": 0, "parent":self, "args": args || [] };
+          defaults = {
+            "autostart": true,
+            "start": ~~elapsedTime,
+            "stop": Infinity, 
+            "interval": Infinity, 
+            "maxIntervals": Infinity, 
+            "duration": 0, 
+            "calledAt": [],
+            "startDelay": 0
+          },
+          required = {
+            "name":uniqueName,
+            "fn": fn,
+            "calls": 0,
+            "parent":self,
+            "args": args || []
+          };
 
-      // the loop object
+      // the loop object that will be returned
       var Loop = function(options) {
 
+
         extend(this, defaults, required);
+
         // the check to determine if the loop is called
-        this.check = function(time) {
+        this.checkInterval = function(now) {
           var start = this.start,
               interval = this.interval,
-              duration = this.duration;
+              duration = this.duration,
+              doesIntervalMatchNow = (now - (start + this.startDelay))  % (interval + duration),
+              intervalHasPassed = now - (this.lastCall === undefined ? start : this.lastCall);
+    
 
-          if ( ( (time  -  start) )  % (interval + duration) === 0 ) return true;
+          console.clear();
+          console.log("name:", this.name);
+          console.log("Delta:", self.delta );
+          console.log("-----");
+          console.log("start:", start);
+          console.log("doesIntervalMatchNow", doesIntervalMatchNow);
+          console.log("interval:", interval);
+          console.log("now:", now);
+          console.log("lastCall'ed:", this.lastCall);
+          
+          if ( doesIntervalMatchNow >= 0 && doesIntervalMatchNow <= delta && (now - this.lastCall) > (interval*0.99) ) return true;
           else return false;
         };
 
         this.for = function(options) {
           extend(this, options);
-          return this.parent;
+          this.lastCall = this.start - this.interval;
+          return this;
         };
 
         this.delete = false;
@@ -392,11 +509,6 @@
       events.loops[ l.name ] = l;
 
       return l;
-    };
-
-    this.after = function(seconds, fn /* [, args, context ] */) {
-      var args = arguments[2], context = arguments[3];
-      this.at(elapsedSeconds + seconds, fn, args, context);
     };
 
     this.remove = function(event) {
@@ -431,7 +543,9 @@
       // waits for the current stack to clear
       window.setTimeout(0, fn)
     };
-    this.viewEvents = function(){
+
+    this.getEvents = function() {
+      // in case someone wants to view the internally set events
       return events;
     };
 
@@ -444,6 +558,7 @@
       Useful? Could be. Invasive of global prototypes? Def.
 
     * * * * * * * * * * * * * */
+    
     if (options.bindToFunction === true) {
 
         var _t = this; // reference, since wait is in the context of the callee
--- example/example.js
+++ example/example.js
@@ -4,123 +4,181 @@
 
 /*** If you're just looking for ClockEventAggreggator's usage, it is used on line 109 ***/
 
-  var clock = new ClockEventAggreggator({useRAF:true, debug:false});
 
+  var clock = new ClockEventAggreggator({autostart:false, useRAF:true, debug:false}),
+      start = document.getElementById("start"),
+      stop = document.getElementById("stop"),
+      pause = document.getElementById("pause"),
+      resume = document.getElementById("resume"),
+      render;
+
+  
+
+  // button controls
+  start.addEventListener("click", function() {
+    var count = 0;
+
+    clock.at([2000, 4000 ], function(){ document.getElementById("debug").innerHTML += count + "<br />";count++; clock.pause()});
     clock.start();
+    start.disabled = true;
+    stop.removeAttribute("disabled");
+    pause.removeAttribute("disabled");
+    resume.removeAttribute("disabled");
+    
+    clock.on("tick", render);// "tick" is a built in event
+  /*
+    var loop = clock.loop("test", function(){
 
-console.log("Example of https://github.com/RoryDuncan/ClockEventAggreggator/")
+        var count = this.calledAt.length;
+        document.getElementById("debug").innerHTML += "Loop " + (count-1) + " called at " + this.calledAt[count-1] + "ms<br />";
+
+
+    }).for({"interval":1000, "maxIntervals":50});
+  */
+  
+    
+  });
+
+
+  stop.addEventListener("click", function() {
+    clock.stop();
+    start.removeAttribute("disabled");
+    stop.disabled = true;
+    pause.disabled = true;
+    resume.disabled = true;
+  });
+
+
+  pause.addEventListener("click", function() {
+    clock.pause();
+  });
+
+  resume.addEventListener("click", function() {
+    clock.resume();
+  });
+
+
+console.log("%cExample of http://github.com/RoryDuncan/ClockEventAggreggator/", "color:#aaa;");
 // a wrapper
-var example = function() {
-
-  // get the canvas from the DOM
-  var canvas = document.getElementById("renderExample"),
-
-      // and the context, ctx is easier to type
-      ctx = canvas.getContext("2d"),
-
-      // pi, with some extra decimals
-      pi = 22 / 7;
-
-      // fixed size
-      canvas.width = canvas.height = 700;
-      
-      // create a 'center' object for ease-of-access.
-      var center = {
-        x: canvas.width / 2,
-        y: canvas.height / 2
-      };
-
-  // updateTicksHand updates the display of the 'hand' of the clock that represents ClockEventAggreggator's tick.
-  var updateTicksHand = function() {
-      // set the radius to 40% of the canvas's width
-      var radius = canvas.width * 0.4;
 
 
-      // set a background rectangle
-      ctx.fillStyle = "#fff";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+// get the canvas from the DOM
+var canvas = document.getElementById("renderExample"),
 
-      // start drawing a line from the center
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
+    // and the context, ctx is easier to type
+    ctx = canvas.getContext("2d"),
 
-      // thicken the line
-      ctx.lineWidth = 3;
+    // pi, with some extra decimals
+    pi = 22 / 7;
 
-      // set the positions of the end of the line to the position of the circle radius relevant to ticks
-      // note: timeline.
-      var x = radius * Math.cos( clock.ticks() / 2 ) + center.x,
-          y = radius * Math.sin( clock.ticks() / 2 ) + center.y;
+    // fixed size
+    canvas.width = canvas.height = 350;
+    
+    // create a 'center' object for ease-of-access.
+    var center = {
+      x: canvas.width / 2,
+      y: canvas.height / 2
+    };
 
-      //set the tick hand's color
-      ctx.strokeStyle = "#088";
-
-      // draw the line
-      ctx.lineTo(x, y);
-
-      // stroke / fill it in
-      ctx.stroke();
-  };
-
-  // the only difference between updateSecondsHand and updateTicksHand is the color and linewidth of the 'hand'
-  var updateSecondsHand = function() {
-
-      var radius = canvas.width * 0.4;
-
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
-
-      ctx.strokeStyle = "#f44";
-      ctx.lineWidth = 5;
-
-      var x = center.x + ( radius * Math.cos( clock.seconds() + center.x )),
-          y = center.y + ( radius * Math.sin( clock.seconds() + center.y ));
-      ctx.lineTo(x, y);
-      ctx.stroke();
-  };
-
-  // the only difference between updateMinutesHand and updateTicksHand is the color and linewidth of the 'hand'
-  var updateMinutesHand = function() {
-
-      var radius = canvas.width * 0.4;
-
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
-
-      ctx.strokeStyle = "#000";
-      ctx.lineWidth = 8;
-
-      var x = center.x + ( radius * Math.cos(( clock.seconds() / 60) + center.x )),
-          y = center.y + ( radius * Math.sin(( clock.seconds() / 60) + center.y ));
-      ctx.lineTo(x, y);
-      ctx.stroke();
-  };
-
-  var adjustClockSpeed = function(){
-    var input = document.getElementById("clockspeed").value;
-    document.getElementById("clockvalue").innerHTML = input;
-    clock.setClockSpeed(input);
-
-  };
-
-  // render is the wrapper that will call the uodates
-  var render = function() {
-
-    //console.log("%cRENDERS", "background-color: #88c; padding: 1px 5px; color: #fff; border-radius: 10px; font-size: 10px; font-weight: bold; position:");
-    //console.log(clock.seconds() )
-    updateTicksHand();
-    updateSecondsHand();
-    updateMinutesHand();
-    adjustClockSpeed();
+// updateTicksHand updates the display of the 'hand' of the clock that represents ClockEventAggreggator's tick.
+var updateTicksHand = function() {
+    // set the radius to 40% of the canvas's width
+    var radius = canvas.width * 0.4;
 
 
-  };
+    // set a background rectangle
+    ctx.fillStyle = "#fff";
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 1;
+    // start drawing a line from the center
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
 
+    // thicken the line
+    ctx.lineWidth = 3;
 
-  // use the ClockEventAggreggator's "on" method
-  clock.on("tick", render);
+    // set the positions of the end of the line to the position of the circle radius relevant to ticks
+    // note: timeline.
+    var x = radius * Math.cos( clock.ticks() / 2 ) + center.x,
+        y = radius * Math.sin( clock.ticks() / 2 ) + center.y;
+
+    //set the tick hand's color
+    ctx.strokeStyle = "#088";
+
+    // draw the line
+    ctx.lineTo(x, y);
+
+    // stroke / fill it in
+    ctx.stroke();
+};
+
+// the only difference between updateSecondsHand and updateTicksHand is the color and linewidth of the 'hand'
+var updateSecondsHand = function() {
+
+    var radius = canvas.width * 0.4;
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
+
+    ctx.strokeStyle = "#f44";
+    ctx.lineWidth = 5;
+
+    var x = center.x + ( radius * Math.cos( clock.seconds() + center.x )),
+        y = center.y + ( radius * Math.sin( clock.seconds() + center.y ));
+    ctx.lineTo(x, y);
+    ctx.stroke();
+};
+
+// the only difference between updateMinutesHand and updateTicksHand is the color and linewidth of the 'hand'
+var updateMinutesHand = function() {
+
+    var radius = canvas.width * 0.4;
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 , canvas.height / 2 );
+
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 8;
+
+    var x = center.x + ( radius * Math.cos(( clock.minutes() ) + center.x )),
+        y = center.y + ( radius * Math.sin(( clock.minutes() ) + center.y ));
+    ctx.lineTo(x, y);
+    ctx.stroke();
+};
+
+var adjustClockSpeed = function(){
+  var input = document.getElementById("clockspeed").value;
+  document.getElementById("clockvalue").innerHTML = input;
+  clock.setClockSpeed(input);
 
 };
 
+  // render is the wrapper that will call the uodates
+
+
+  var render = function() {
+  
+  var ms = ~~(clock.milliseconds() % 1000),
+      secs = ~~clock.seconds(),
+      mins = ~~clock.minutes(),
+      hours = ~~(clock.minutes() % 60);
+
+  var time = hours + ":" + mins + ":" + secs + ":" + ms;
+
+  updateTicksHand();
+  updateSecondsHand();
+  updateMinutesHand();
+  adjustClockSpeed();
+
+  document.getElementById("time").innerHTML = "CLOCK: " + time + "<br />";
+
+
+};
+render();
+
+// use the ClockEventAggreggator's "on" method
+clock.on("tick", render);
+
 // call the function we wrapped.
-example();
+
